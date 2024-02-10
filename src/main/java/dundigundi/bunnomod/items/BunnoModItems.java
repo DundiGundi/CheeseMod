35f41328c3c59a5e4fe8947b5e20b41333a5845b
DundiGundi <gundiafan@gmail.com>
2024-02-10 17:48:45 +0100

    added config system

--- src/main/java/dundigundi/bunnomod/items/BunnoModItems.java
+++ src/main/java/dundigundi/bunnomod/items/BunnoModItems.java
@@ -2,6 +2,7 @@ package dundigundi.bunnomod.items;
 
 
 import dundigundi.bunnomod.BunnoMod;
+import dundigundi.bunnomod.BunnoModConfig;
 import net.minecraft.core.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,12 +13,15 @@ public class BunnoModItems {
 	public static final String MOD_ID = BunnoMod.MOD_ID;
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	private int nextItemID(String itemName) {
+		return BunnoModConfig.cfg.getInt("Item IDs." + itemName);
+	}
+
 	//Ore
 	public static Item brownCoal;
 
 	public void initializeItems() {
-		int startingItemId = 16600;
 
-		brownCoal = ItemHelper.createItem(MOD_ID, new Item(startingItemId++), "brownCoal", "brownCoal.png");
+		brownCoal = ItemHelper.createItem(MOD_ID, new Item(nextItemID("brownCoal")), "brownCoal", "brownCoal.png");
 	}
 }

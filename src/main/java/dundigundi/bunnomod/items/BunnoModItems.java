5f20c8eed6b492d28c0b6c1bc524e274afbfbd86
DundiGundi <gundiafan@gmail.com>
2024-02-10 11:43:21 +0100

    cleanup, made names more consistent

--- src/main/java/dundigundi/bunnomod/items/BunnoModItems.java
+++ src/main/java/dundigundi/bunnomod/items/BunnoModItems.java
@@ -1,22 +1,11 @@
 package dundigundi.bunnomod.items;
 
-import com.mojang.nbt.Tag;
+
 import dundigundi.bunnomod.BunnoMod;
-import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.sound.block.BlockSounds;
-import net.minecraft.core.block.Block;
-import net.minecraft.core.block.material.Material;
-import net.minecraft.core.block.tag.BlockTags;
-import net.minecraft.core.crafting.LookupFuelFurnace;
 import net.minecraft.core.item.Item;
-import net.minecraft.core.item.ItemCoal;
-import net.minecraft.core.item.tag.ItemTags;
-import net.minecraft.core.item.tool.ItemToolPickaxe;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.helper.BlockBuilder;
 import turniplabs.halplibe.helper.ItemHelper;
-import turniplabs.halplibe.util.GameStartEntrypoint;
 
 
 public class BunnoModItems {
@@ -29,6 +18,6 @@ public class BunnoModItems {
 	public void initializeItems() {
 		int startingItemId = 16600;
 
-		oreBrownCoal = ItemHelper.createItem(MOD_ID, new Item(startingItemId++), "ore.brownCoal", "oreBrownCoal.png");
+		oreBrownCoal = ItemHelper.createItem(MOD_ID, new Item(startingItemId++), "brownCoal", "brownCoal.png");
 	}
 }

2d168299207deb01ba850469901450132528ab4e
DundiGundi <gundiafan@gmail.com>
2024-02-11 16:56:25 +0100

    made some changes to todo list and a comment in BlockBlockofCheese.java

--- src/main/java/dundigundi/bunnomod/blocks/BlockBlockofCheese.java
+++ src/main/java/dundigundi/bunnomod/blocks/BlockBlockofCheese.java
@@ -2,11 +2,10 @@ package dundigundi.bunnomod.blocks;
 
 import dundigundi.bunnomod.BunnoMod;
 import dundigundi.bunnomod.items.BunnoModItems;
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.block.BlockCake;
 import net.minecraft.core.block.entity.TileEntity;
+import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.enums.EnumDropCause;
-import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.World;
@@ -19,6 +18,29 @@ public class BlockBlockofCheese extends BlockCake {
 	@Override
 	public ItemStack[] getBreakResult(World world, EnumDropCause dropCause, int x, int y, int z, int meta, TileEntity tileEntity) {
 		return dropCause != EnumDropCause.PICK_BLOCK && meta != 0 ? null : new ItemStack[]{new ItemStack(BunnoModBlocks.blockOfCheese)};
+	}
+	@Override
+	public boolean blockActivated(World world, int x, int y, int z, EntityPlayer player) {
+		this.eatCakeSlice(world, x, y, z, player);
+		return true;
+	}
+	@Override
+	public void onBlockClicked(World world, int x, int y, int z, EntityPlayer player) {
+		this.eatCakeSlice(world, x, y, z, player);
+	}
+	private void eatCakeSlice(World world, int i, int j, int k, EntityPlayer entityplayer) {
+		if (entityplayer.health < 20) {
+			entityplayer.heal(2);
+			//entityplayer.inventory.insertItem(new ItemStack(BunnoModItems.cheeseSlice),false);  player gets a slice of cheese
+			int l = world.getBlockMetadata(i, j, k) + 1;
+			if (l >= 6) {
+				world.setBlockWithNotify(i, j, k, 0);
+			} else {
+				world.setBlockMetadataWithNotify(i, j, k, l);
+				world.markBlockDirty(i, j, k);
+			}
+		}
+
 	}
 	@Override
 	public int getBlockTextureFromSideAndMetadata(Side side, int data) {
